d6440ac1dad32d0f673c383169db0d5779aca226
Ilya <107246761+illya-s@users.noreply.github.com>
2023-06-14 13:34:14 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-// const handleChange = (isChecked) => {
-// 	if (isChecked) {
-// 		document.body.setAttribute('dark', '')
-// 	} else {
-// 		document.body.removeAttribute('dark')
-// 	}
-// }
-
 VANTA.RINGS({
   el: "#intro",
   mouseControls: true,
   touchControls: true,
   gyroControls: true,
-  minHeight: 275.00,
-  minWidth: 275.00,
-  scale: 1.00,
-  scaleMobile: 0.50
+  scale: 2.00,
+  scaleMobile: 1.00
 })
