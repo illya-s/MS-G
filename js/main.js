0c309874a4bd86e4af279a62683b6db67a4ae617
Ilya <107246761+illya-s@users.noreply.github.com>
2024-05-09 20:40:15 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-VANTA.RINGS({
-  el: "#intro",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: true,
-  scale: 1.00,
-  scaleMobile: 1.00
-})
+$(document).ready(function() {
+	var userAgent = navigator.userAgent.toLowerCase();
+	if (userAgent.indexOf('firefox') > -1) {
+		$("html").css("scrollbar-width", "thin")
+		$("html").css("scrollbar-color", "var(--bn-color) var(--bg-color)")
+	}
+
+	VANTA.RINGS({
+		el: "#intro",
+		mouseControls: true,
+		touchControls: true,
+		gyroControls: true,
+		scale: 1.00,
+		scaleMobile: 1.00
+	})
+});
